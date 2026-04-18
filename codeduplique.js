1b328a4e55fc065e4dffb0217270b3e23a1030e9
Elouanleconfessonosse <tourtoulouloulou@gmail.com>
2026-04-18 18:15:29 +0200

    Add files via upload

---
+++ codeduplique.js
@@ -0,0 +1,388 @@
+﻿const tactique = {
+    "4-4-2": [
+        {position: "attaquant1", top: "14%", left: "45%"},
+        {position: "attaquant2", top: "14%", left: "55%"},
+        {position: "milieu1", top: "28%", left: "35%"},
+        {position: "milieu2", top: "28%", left: "45%"},
+        {position: "milieu3", top: "28%", left: "55%"},
+        {position: "milieu4", top: "28%", left: "65%"},
+        {position: "defenseur1", top: "40%", left: "35%"},
+        {position: "defenseur2", top: "45%", left: "45%"},
+        {position: "defenseur3", top: "45%", left: "55%"},
+        {position: "defenseur4", top: "40%", left: "65%"},
+        {position: "gardien", top: "54%", left: "50%"}
+    ],
+    "4-3-3": [
+        {position: "attaquant1", top: "16%", left: "40%"},
+        {position: "attaquant2", top: "12%", left: "50%"},
+        {position: "attaquant3", top: "16%", left: "60%"},
+        {position: "milieu1", top: "30%", left: "40%"},
+        {position: "milieu2", top: "33%", left: "50%"},
+        {position: "milieu3", top: "30%", left: "60%"},
+        {position: "defenseur1", top: "40%", left: "35%"},
+        {position: "defenseur2", top: "45%", left: "45%"},
+        {position: "defenseur3", top: "45%", left: "55%"},
+        {position: "defenseur4", top: "40%", left: "65%"},
+        {position: "gardien", top: "54%", left: "50%"}
+    ],
+    "Perignat": [
+        {position: "attaquant1", top: "14%", left: "45%"},
+        {position: "attaquant2", top: "14%", left: "55%"},
+        {position: "milieu1", top: "26%", left: "30%"},
+        {position: "milieu2", top: "29%", left: "40%"},
+        {position: "milieu3", top: "31%", left: "50%"},
+        {position: "milieu4", top: "29%", left: "60%"},
+        {position: "milieu5", top: "26%", left: "70%"},
+        {position: "defenseur1", top: "45%", left: "40%"},
+        {position: "defenseur2", top: "45%", left: "50%"},
+        {position: "defenseur3", top: "45%", left: "60%"},
+        {position: "gardien", top: "54%", left: "50%"}
+    ],
+    "5-3-2": [
+        {position: "attaquant1", top: "14%", left: "45%"},
+        {position: "attaquant2", top: "14%", left: "55%"},
+        {position: "milieu1", top: "30%", left: "40%"},
+        {position: "milieu2", top: "30%", left: "50%"},
+        {position: "milieu3", top: "30%", left: "60%"},
+        {position: "defenseur1", top: "39%", left: "32%"},
+        {position: "defenseur2", top: "45%", left: "41%"},
+        {position: "defenseur3", top: "47%", left: "50%"},
+        {position: "defenseur4", top: "45%", left: "59%"},
+        {position: "defenseur5", top: "39%", left: "68%"},
+        {position: "gardien", top: "54%", left: "50%"}
+    ],
+    "4-2-3-1": [
+        {position: "attaquant1", top: "10%", left: "50%"},
+        {position: "milieu1", top: "20%", left: "38%"},
+        {position: "milieu2", top: "18%", left: "50%"},
+        {position: "milieu3", top: "20%", left: "62%"},
+        {position: "milieu4", top: "32%", left: "45%"},
+        {position: "milieu5", top: "32%", left: "55%"},
+        {position: "defenseur1", top: "40%", left: "35%"},
+        {position: "defenseur2", top: "45%", left: "45%"},
+        {position: "defenseur3", top: "45%", left: "55%"},
+        {position: "defenseur4", top: "40%", left: "65%"},
+        {position: "gardien", top: "54%", left: "50%"}
+    ],
+    "4-1-4-1": [
+        {position: "attaquant1", top: "12%", left: "50%"},
+        {position: "milieu1", top: "25%", left: "35%"},
+        {position: "milieu2", top: "25%", left: "45%"},
+        {position: "milieu3", top: "25%", left: "55%"},
+        {position: "milieu4", top: "25%", left: "65%"},
+        {position: "milieu5", top: "34%", left: "50%"},
+        {position: "defenseur1", top: "40%", left: "35%"},
+        {position: "defenseur2", top: "45%", left: "45%"},
+        {position: "defenseur3", top: "45%", left: "55%"},
+        {position: "defenseur4", top: "40%", left: "65%"},
+        {position: "gardien", top: "54%", left: "50%"}
+    ],
+    "4-5-1": [
+        {position: "attaquant1", top: "12%", left: "50%"},
+        {position: "milieu1", top: "26%", left: "30%"},
+        {position: "milieu2", top: "28%", left: "40%"},
+        {position: "milieu3", top: "30%", left: "50%"},
+        {position: "milieu4", top: "28%", left: "60%"},
+        {position: "milieu5", top: "26%", left: "70%"},
+        {position: "defenseur1", top: "40%", left: "35%"},
+        {position: "defenseur2", top: "45%", left: "45%"},
+        {position: "defenseur3", top: "45%", left: "55%"},
+        {position: "defenseur4", top: "40%", left: "65%"},
+        {position: "gardien", top: "54%", left: "50%"}
+    ],
+    "5-4-1": [
+        {position: "attaquant1", top: "12%", left: "50%"},
+        {position: "milieu1", top: "28%", left: "35%"},
+        {position: "milieu2", top: "28%", left: "45%"},
+        {position: "milieu3", top: "28%", left: "55%"},
+        {position: "milieu4", top: "28%", left: "65%"},
+        {position: "defenseur1", top: "39%", left: "32%"},
+        {position: "defenseur2", top: "45%", left: "41%"},
+        {position: "defenseur3", top: "47%", left: "50%"},
+        {position: "defenseur4", top: "45%", left: "59%"},
+        {position: "defenseur5", top: "39%", left: "68%"},
+        {position: "gardien", top: "54%", left: "50%"}
+    ],
+    "3-3-1": [
+        {position: "attaquant1", top: "16%", left: "50%"},
+        {position: "milieu1", top: "28%", left: "38%"},
+        {position: "milieu2", top: "32%", left: "50%"},
+        {position: "milieu3", top: "28%", left: "62%"},
+        {position: "defenseur1", top: "40%", left: "35%"},
+        {position: "defenseur2", top: "45%", left: "50%"},
+        {position: "defenseur3", top: "40%", left: "65%"},
+        {position: "gardien", top: "54%", left: "50%"}
+    ],
+    "3-2-2": [
+        {position: "attaquant1", top: "16%", left: "42%"},
+        {position: "attaquant2", top: "16%", left: "58%"},
+        {position: "milieu1", top: "32%", left: "42%"},
+        {position: "milieu2", top: "32%", left: "58%"},
+        {position: "defenseur1", top: "40%", left: "35%"},
+        {position: "defenseur2", top: "45%", left: "50%"},
+        {position: "defenseur3", top: "40%", left: "65%"},
+        {position: "gardien", top: "54%", left: "50%"}
+    ],
+    "2-3-2": [
+        {position: "attaquant1", top: "16%", left: "42%"},
+        {position: "attaquant2", top: "16%", left: "58%"},
+        {position: "milieu1", top: "28%", left: "38%"},
+        {position: "milieu2", top: "32%", left: "50%"},
+        {position: "milieu3", top: "28%", left: "62%"},
+        {position: "defenseur1", top: "44%", left: "42%"},
+        {position: "defenseur2", top: "44%", left: "58%"},
+        {position: "gardien", top: "54%", left: "50%"}
+    ],
+    "4-2-1": [
+        {position: "attaquant1", top: "16%", left: "50%"},
+        {position: "milieu1", top: "30%", left: "42%"},
+        {position: "milieu2", top: "30%", left: "58%"},
+        {position: "defenseur1", top: "39%", left: "32%"},
+        {position: "defenseur2", top: "44%", left: "42%"},
+        {position: "defenseur3", top: "44%", left: "58%"},
+        {position: "defenseur4", top: "39%", left: "68%"},
+        {position: "gardien", top: "54%", left: "50%"}
+    ],
+    "2-4-1": [
+        {position: "attaquant1", top: "15%", left: "50%"},
+        {position: "milieu1", top: "26%", left: "32%"},
+        {position: "milieu2", top: "30%", left: "44%"},
+        {position: "milieu3", top: "30%", left: "56%"},
+        {position: "milieu4", top: "26%", left: "68%"},
+        {position: "defenseur1", top: "44%", left: "42%"},
+        {position: "defenseur2", top: "44%", left: "58%"},
+        {position: "gardien", top: "54%", left: "50%"}
+    ]
+};
+const cartefifas = {
+    'S25FCPro': 'Image/S25FCPro.png',
+    'S26FUTHeroes': 'Image/S26FUTHeroes.png',
+    'S26TOTW': 'Image/S26TOTW.png',
+    'S26IconLegend': 'Image/S26IconLegend.png',
+    'S25TOTY': 'Image/S25TOTY.png',
+    'S24TOTY': 'Image/S24TOTY.png',
+    'S25ChampionsLeague': 'Image/S25ChampionsLeague.png',
+    'S24IconLegend': 'Image/S24IconLegend.png'
+};
+const drapeaux = {
+    'France': "https://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_France.svg",
+    'Angleterre': "https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg",
+    'Espagne': "https://upload.wikimedia.org/wikipedia/en/9/9a/Flag_of_Spain.svg",
+    'Italie': "https://upload.wikimedia.org/wikipedia/en/0/03/Flag_of_Italy.svg",
+    'Allemagne': "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Germany_%283-2%29.svg/250px-Flag_of_Germany_%283-2%29.svg.png",
+    'Portugal': "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg",
+    'Belgique': "https://upload.wikimedia.org/wikipedia/commons/6/65/Flag_of_Belgium.svg",
+    'Bresil': "https://upload.wikimedia.org/wikipedia/en/0/05/Flag_of_Brazil.svg",
+    'Argentine': "https://upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg",
+    'Uruguay': "https://upload.wikimedia.org/wikipedia/commons/f/fe/Flag_of_Uruguay.svg",
+    'Maroc': "https://upload.wikimedia.org/wikipedia/commons/2/2c/Flag_of_Morocco.svg",
+    'Albanie': "https://upload.wikimedia.org/wikipedia/commons/3/36/Flag_of_Albania.svg",
+    'Finlande': "https://upload.wikimedia.org/wikipedia/commons/b/bc/Flag_of_Finland.svg",
+    'USA': "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg",
+    'Chili': "https://upload.wikimedia.org/wikipedia/commons/7/78/Flag_of_Chile.svg",
+    'Canada': "https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Canada_%28Pantone%29.svg",
+    'Colombie': "https://upload.wikimedia.org/wikipedia/commons/2/21/Flag_of_Colombia.svg",
+    'Japon': "https://upload.wikimedia.org/wikipedia/en/9/9e/Flag_of_Japan.svg",
+    'Senegal': "https://upload.wikimedia.org/wikipedia/commons/f/fd/Flag_of_Senegal.svg",
+    'Pays-Bas': "https://upload.wikimedia.org/wikipedia/commons/2/20/Flag_of_the_Netherlands.svg"
+};
+const cheveux = {
+    'Blond': 'Image/cheveuxblond.png',
+    'Brun': 'Image/cheveuxbrun.png',
+    'Roux': 'Image/cheuveuxroue.png',
+    'Locks': 'Image/cheveuxl.png',
+    'Loks': 'Image/cheveuxl.png'
+};
+const texteh2 = document.querySelector(".information h2");
+const containerinfo = document.querySelector(".containerInfo");
+
+function informationErreur(message) {
+  texteh2.textContent = message;
+  containerinfo.style.display = "flex";
+  setTimeout(() => {
+    containerinfo.style.display = "none";
+  }, 3000);
+}
+    function fermerInformation(){
+        containerinfo.style.display = "none";
+    }
+const titres = [
+    {musique: "Musique/Man I Need.mp3", titreMusique: "Man I Need"},
+    {musique: "Musique/MarvinGaye.mp3", titreMusique: "Ain't No Mountain High Enough"},
+    {musique: "Musique/On My Soul.mp3", titreMusique: "On My Soul"},
+    {musique: "Musique/Sunflower.mp3", titreMusique: "Sunflower"},
+    {musique: "Musique/April Towers.mp3", titreMusique: "April Towers"},
+    {musique: "Musique/Atlas Genius.mp3", titreMusique: "Atlas Genius"},
+    {musique: "Musique/All Tvvins.mp3", titreMusique: "All Tvvins"},
+    {musique: "Musique/Beck.mp3", titreMusique: "Beck"},
+    {musique: "Musique/Bastille.mp3", titreMusique: "Bastille"},
+    {musique: "Musique/Banners.mp3", titreMusique: "Banners"},
+    {musique: "Musique/Baio.mp3", titreMusique: "Baio"},
+    {musique: "Musique/BaianaSystem.mp3", titreMusique: "BaianaSystem"},
+    {musique: "Musique/AURORA.mp3", titreMusique: "AURORA"},
+    {musique: "Musique/Bomba Estéreo.mp3", titreMusique: "Bomba Estéreo"},
+    {musique:"Musique/Che Lingo - Eyes On The Prize ft. Tamaraebi (Official Music Video).mp3", titreMusique:"Eyes On The Price"},
+    {musique:"Musique/Glass Animals - I Dont Wanna Talk (I Just Wanna Dance)  Official Video.mp3", titreMusique:"I Dont Wanna Talk"},
+    {musique:"Musique/Joy Crookes - Feet Dont Fail Me Now (Official Video).mp3", titreMusique:"Feet Dont Fail"},
+    {musique:"Musique/Smallpools - Dreaming (Official Video).mp3", titreMusique:"Dreaming"},
+    {musique:"Musique/Pip Millett - Make Me Cry  A COLORS SHOW.mp3", titreMusique:"Make Me Cry  A COLORS SHOW"},
+    {musique:"Musique/Hak Baker - Cool Kids.mp3", titreMusique:"Cool Kids"},
+    {musique:"Musique/Greentea Peng - Stuck In The Middle (Official Video).mp3", titreMusique:"Stuck In The Middle"},
+    {musique:"Musique/Greentea Peng - Free My People (Official Video) ft. Simmy, Kid Cruise.mp3", titreMusique:"Free My People"},
+    {musique:"Musique/Greentea Peng - DOWNERS.mp3", titreMusique:"DOWNERS"},
+    {musique:"Musique/Classic.mp3", titreMusique:"Classic"},
+    {musique:"Musique/OSHUN - Solar Plexus ft. Proda  A COLORS SHOW.mp3", titreMusique:"Solar Plexus"},
+    {musique:"Musique/Think i m in love with you.mp3", titreMusique:"Think I'm In Love With You"},
+    {musique:"Musique/Clean Bandit - Symphony (feat. Zara Larsson) [Official Video].mp3", titreMusique:"Symphony"}
+
+];
+let timePosition = 0;
+let index = 0;
+const audio = document.getElementById("audio");
+audio.volume = 0.4;
+const musiquePlayKey = "musiqueplay";
+if(localStorage.getItem("musiqueVolume")){
+    audio.volume = parseFloat(localStorage.getItem("musiqueVolume"));
+}
+if (localStorage.getItem("musiqueIndex")) {
+    index = parseInt(localStorage.getItem("musiqueIndex"));
+}
+if (localStorage.getItem("musiquePosition")) {
+    timePosition = parseFloat(localStorage.getItem("musiquePosition"));
+}
+function montervolume(){
+    audio.volume += 0.1;
+    localStorage.setItem("musiqueVolume", audio.volume);
+if(audio.volume < 0.3){
+      volumebouton.textContent = "🔈";
+}
+else if(audio.volume > 0.7){
+    volumebouton.textContent = "🔊";
+}
+else if(audio.volume > 0.3 && audio.volume < 0.7){
+    volumebouton.textContent = "🔉";
+}
+}
+function descendrevolume(){
+    audio.volume -= 0.1;
+    localStorage.setItem("musiqueVolume", audio.volume);  
+if(audio.volume < 0.3 && audio.volume > 0){
+      volumebouton.textContent = "🔈";
+}
+else if(audio.volume > 0.7){
+    volumebouton.textContent = "🔊";
+}
+else if(audio.volume > 0.3 && audio.volume < 0.7){
+    volumebouton.textContent = "🔉";
+}
+else if(audio.volume <= 0){
+    volumebouton.textContent = "🔇";
+}
+}
+function coupervolume(){
+    audio.volume = 0;
+    localStorage.setItem("musiqueVolume", audio.volume);
+    volumebouton.textContent = "🔇";
+}
+const volumebouton = document.getElementById("volumeshut");
+const volumeup = document.getElementById("volumeup");
+const volumedown = document.getElementById("volumedown");
+if(audio.volume <= 0){
+        volumebouton.textContent = "🔇";
+}    
+else if(audio.volume < 0.3 && audio.volume > 0){
+      volumebouton.textContent = "🔈";
+}
+else if(audio.volume > 0.7){
+    volumebouton.textContent = "🔊";
+}
+else if(audio.volume > 0.3 && audio.volume < 0.7){
+    volumebouton.textContent = "🔉";
+}
+const spotizerelement = document.querySelector(".spotizer");
+        
+        const button1 = document.querySelectorAll(".spotizer button");
+const texteSpotizer = document.querySelector(".spotizer h1");
+function chargerMusique() {
+    audio.src = titres[index].musique;
+    audio.currentTime = timePosition;
+    texteSpotizer.textContent = titres[index].titreMusique;
+    localStorage.setItem("musiqueIndex", index);
+    localStorage.setItem("musiquePosition", audio.currentTime);
+}
+
+function lancerlamusique() {
+    chargerMusique();
+    audio.play();
+    logoPause.textContent = "||";
+    localStorage.setItem(musiquePlayKey, "1");
+}
+const logoPause = document.getElementById("pauseouplay");
+
+function mettreEnPause() {
+    audio.pause();
+    logoPause.textContent = "▶";
+    localStorage.setItem(musiquePlayKey, "0");
+}
+function lancerouarreterlamusique() {
+    if (audio.paused) {
+        lancerlamusique();
+    } else {
+        mettreEnPause();
+    }
+}
+function musiquesuivante() {
+    index = (index + 1) % titres.length;
+    timePosition = 0;
+    localStorage.setItem("musiquePosition", timePosition);
+    localStorage.setItem("musiqueIndex", index);
+    if (localStorage.getItem(musiquePlayKey) === "1") {
+        lancerlamusique();
+    } else {
+        chargerMusique();
+        logoPause.textContent = "▶";
+    }
+}
+function musiquePrecedente() {
+    index = (index - 1 + titres.length) % titres.length;
+    timePosition = 0;
+    localStorage.setItem("musiquePosition", timePosition);
+    localStorage.setItem("musiqueIndex", index);
+    if (localStorage.getItem(musiquePlayKey) === "1") {
+        lancerlamusique();
+    } else {
+        chargerMusique();
+        logoPause.textContent = "▶";
+    }
+}
+window.addEventListener("beforeunload", function() {
+    localStorage.setItem("musiqueIndex", index);
+    localStorage.setItem("musiquePosition", audio.currentTime);
+    localStorage.setItem(musiquePlayKey, audio.paused ? "0" : "1");
+});
+audio.addEventListener("ended", function() {
+    index = (index + 1) % titres.length;
+    timePosition = 0;
+    localStorage.setItem("musiqueIndex", index);
+    localStorage.setItem("musiquePosition", timePosition);
+    lancerlamusique();
+});
+if (localStorage.getItem("musiqueIndex")) {
+    if (localStorage.getItem(musiquePlayKey) === "1") {
+        lancerlamusique();
+    } else {
+        chargerMusique();
+        logoPause.textContent = "▶";
+    }
+}  
+const html = document.querySelector("html");
+const containerInfo = document.querySelector(".containerInfo");
+html.addEventListener("click", () => {
+    if (containerInfo.style.display === "flex"){
+        containerInfo.style.display = "none";
+    }
+});
+document.querySelector(".fond").style.display = "flex";
+setTimeout(() =>{
+    document.querySelector(".fond").style.display ="none";
+},3000);
